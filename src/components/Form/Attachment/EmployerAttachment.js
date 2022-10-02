d96ff5de20b65e4da3d6a2943fc9be683522c1d3
Benjamin Achebe <bachebe@tsllimited.com>
2022-10-03 00:45:35 +0100

    appending and deletion logic to attachment component

--- src/components/Form/Attachment/EmployerAttachment.js
+++ src/components/Form/Attachment/EmployerAttachment.js
@@ -1,39 +1,75 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 
 const AttachmentUpload = () => {
-    const [attachedFiles, setAttachedFiles] = useState(null);
-    const inputRef = useRef();
+  const [attachedFiles, setAttachedFiles] = useState(null);
+  const inputRef = useRef();
 
-    const clickHandler = () => {
-        inputRef.current.click();
-    };
+  const clickHandler = () => {
+    inputRef.current.click();
+  };
 
-    const preventHandler = (event) => {
-        event.stopPropagation();
-        event.preventDefault();
-      }
+  //Function logic for deleting attached files
+  const removeAttachedFileHandler = (name) => {
+      const newAttachedFiles = attachedFiles.filter((file) => file.name !== name)
+      setAttachedFiles(newAttachedFiles)
+  };
 
-      console.log(attachedFiles);
+  //Function logic for adding new attachment
+  const addFilesHandler = (attachedFileList, mode = 'write') => {
+    if (mode === 'add') {
+      setAttachedFiles((prevAttachedFiles) => [...prevAttachedFiles, ...Array.from(attachedFileList)])
+    } else if (mode === 'write') {
+      setAttachedFiles(Array.from(attachedFileList))
+    }
+  }
+
+  const preventHandler = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+  };
+
+  console.log(attachedFiles);
 
   return (
     <div>
       <div>
         <div>
+            {/*logic for displaying multiple files at once */}
           <h5>Your files:</h5>
-          { attachedFiles && (<ul>{attachedFiles .map((file, index) => (<li key={file.name}>{index + 1}.{file.name}</li>))}</ul>)}
+          {attachedFiles && (
+            <ul>
+              {attachedFiles.map((file, index) => (
+                <li key={file.name}>
+                  {index + 1}.{file.name}
+                  {/*Logic for deleting attached files */}
+                  <span onClick={() => removeAttachedFileHandler(file.name)}>
+                    <i className="fa fa-times" />
+                  </span>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
-        <div onDragEnter={preventHandler} onDragOver={preventHandler} onDrop={(event) => {
-            preventHandler(event)
-            setAttachedFiles(Array.from(event.dataTransfer.files))
-        }}>
+        <div
+          onDragEnter={preventHandler}
+          onDragOver={preventHandler}
+          onDrop={(event) => {
+            preventHandler(event);
+
+            {/*Logic for converting attached files to an array list */}
+            addFilesHandler(event.dataTransfer.files, 'add');
+          }}
+        >
           <i className="fa fa-cloud-upload fa-4x"></i>
           <p>Drag and drop files or select files below.</p>
           <input
             ref={inputRef}
             type="file"
             multiple
-            style={{ display: 'none' }}
-            onChange={(event) => setAttachedFiles(Array.from(event.target.files))}
+            style={{ display: "none" }}
+            onChange={(event) =>
+              addFilesHandler(event.target.files, 'add')
+            }
           />
           <button onClick={clickHandler}>Choose Files</button>
         </div>
